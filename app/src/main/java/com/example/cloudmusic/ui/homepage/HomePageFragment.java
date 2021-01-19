042c6269db6ef48b962847ec62ab15f84a07bb0d
sy <905626971@qq.com>
2021-01-19 14:12:35 +0800

    banner

--- app/src/main/java/com/example/cloudmusic/ui/homepage/HomePageFragment.java
+++ app/src/main/java/com/example/cloudmusic/ui/homepage/HomePageFragment.java
@@ -1,7 +1,22 @@
 package com.example.cloudmusic.ui.homepage;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class HomePageFragment extends Fragment {
+import com.example.cloudmusic.R;
 
+public class HomePageFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+       View view=inflater.inflate(R.layout.fragment_homepage,container,false);
+
+        return view;
+    }
 }
